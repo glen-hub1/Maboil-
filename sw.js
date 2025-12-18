ade1e099caa6eb3f3aa7e82e1b40000018f8d873
Glen mwenyewe <elijahombogo2004@gmail.com>
2025-12-19 00:40:05 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const cacheName = 'maboiller-cache-v1';
+const filesToCache = [
+  './',
+  './index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
